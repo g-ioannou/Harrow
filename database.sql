9ae22713d84e20a20c6ac911d0c1fb8dc7353efa
Giorgos <up1056280@upnet.gr>
2021-08-11 09:53:56 +0300

    Finilised database

--- database.sql
+++ database.sql
@@ -27,7 +27,7 @@ CREATE TABLE files(
 	full_file LONGTEXT,
 	upload_date DATETIME DEFAULT CURRENT_TIMESTAMP(),
 	PRIMARY KEY (file_id),
-	-- FOREIGN KEY (user_id) REFERENCES users(user_id)
+	CONSTRAINT file_user_fk FOREIGN KEY (user_id) REFERENCES users(user_id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -39,7 +39,7 @@ CREATE TABLE entries(
 	startedDateTime DATETIME DEFAULT NULL,
 	wait INT,
 	PRIMARY KEY (entry_id),
-	-- CONSTRAINT entry_file FOREIGN KEY (entry_id) REFERENCES files(file_id)
+	CONSTRAINT entry_file_fk FOREIGN KEY (file_id) REFERENCES files(file_id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -49,8 +49,7 @@ CREATE TABLE requests(
 	url VARCHAR(255),
 	method VARCHAR(5),
 	PRIMARY KEY (request_id),
-	-- FOREIGN KEY (entry_id) REFERENCES entries(entry_id)
-	ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT request_entry_fk FOREIGN KEY (entry_id) REFERENCES entries(entry_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE responses(
@@ -59,7 +58,7 @@ CREATE TABLE responses(
 	statusText TEXT,
 	status INT(3),
 	PRIMARY KEY (response_id),
-	-- FOREIGN KEY (entry_id) REFERENCES entries(entry_id)
+	CONSTRAINT entry_response_fk FOREIGN KEY (entry_id) REFERENCES entries(entry_id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -71,13 +70,127 @@ CREATE TABLE headers(
 	content_type VARCHAR(255) DEFAULT NULL,
 	cache_control VARCHAR(255) DEFAULT NULL,
 	pragma VARCHAR(255) DEFAULT NULL,
-	expires BIGINT DEFAULT NULL,
+	expires VARCHAR(255) DEFAULT NULL,
 	age BIGINT DEFAULT NULL,
 	last_modified VARCHAR(255) DEFAULT NULL,
 	PRIMARY KEY (header_id),
-	-- FOREIGN KEY (request_id) REFERENCES requests(request_id)
-	-- ON DELETE CASCADE ON UPDATE CASCADE,
-	-- FOREIGN KEY (response_id) REFERENCES responses(response_id)
-	-- ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT header_request_fk FOREIGN KEY (request_id) REFERENCES requests(request_id)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT header_response_fk FOREIGN KEY (response_id) REFERENCES responses(response_id)
+	ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+
+
+-- ----------------------------------------------------------
+--  					CREATE PROCEDURES                    
+-- ----------------------------------------------------------
+DROP PROCEDURE IF EXISTS add_user;
+DELIMITER $ 
+CREATE PROCEDURE `add_user` (
+	IN `inp_email` VARCHAR(255),
+	IN `inp_username` VARCHAR(255),
+	IN `inp_password` VARCHAR(255),
+	IN `inp_name` VARCHAR(255),
+	IN `inp_lastname` VARCHAR(255)
+) 
+BEGIN
+	INSERT INTO users (email, username, password, firstname, lastname)
+	VALUES (inp_email,inp_username,inp_password,inp_name,inp_lastname);
+END $ 
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS get_user;
+DELIMITER $ 
+CREATE PROCEDURE `get_user` (IN `inp_email` VARCHAR(255)) 
+BEGIN
+	SELECT *
+	FROM `users`
+	WHERE `email` = inp_email;
+END $
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS validate_login;
+DELIMITER $ 
+CREATE PROCEDURE `validate_login` (
+	IN `inp_email` VARCHAR(255),
+	IN `inp_password` VARCHAR(255)
+) 
+BEGIN
+	SELECT * FROM `users` WHERE `email` = inp_email AND `password` = inp_password;
+END $ 
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_file;
+DELIMITER $
+CREATE PROCEDURE `add_file`(
+	IN `inp_user_id` BIGINT,
+	IN `inp_name` VARCHAR(255),
+	IN `inp_size` VARCHAR(20),
+	IN `inp_upload_isp` VARCHAR(255),
+	IN `inp_upload_location` VARCHAR(255),
+	IN `inp_full_file` LONGTEXT
+)
+BEGIN 
+	INSERT INTO files(user_id,name,size,upload_isp,upload_location,full_file) VALUES (inp_user_id,inp_name,inp_size,inp_upload_isp,inp_upload_location,inp_full_file);
+
+	SELECT MAX(file_id) AS file_id FROM files WHERE name = `inp_name` AND user_id=`inp_user_id`;
+END$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_entry;
+DELIMITER $
+CREATE PROCEDURE `add_entry`(
+	IN `inp_file_id` BIGINT,
+	IN `inp_startedDateTime` VARCHAR(255),
+	IN `inp_serverIpAddress` VARCHAR(20),
+	IN `inp_wait` INT(11)
+)
+
+BEGIN 
+	INSERT INTO entries(file_id,startedDateTime,serverIpAddress,wait) VALUES (inp_file_id,inp_startedDateTime,inp_serverIpAddress,inp_wait);
+
+	SELECT MAX(entry_id) AS entry_id FROM entries WHERE file_id = `inp_file_id` ;
+END$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_request;
+DELIMITER $
+CREATE PROCEDURE `add_request`(
+	IN `inp_entry_id` BIGINT,
+	IN `inp_method` VARCHAR(5),
+	IN `inp_url` VARCHAR(255)
+)
+BEGIN
+	INSERT INTO requests(entry_id,url,method) VALUES (inp_entry_id,inp_url,inp_method);
+
+	SELECT MAX(request_id) AS request_id FROM requests WHERE entry_id = `inp_entry_id` ;
+END$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_response;
+DELIMITER $
+CREATE PROCEDURE `add_response`(
+	IN `inp_entry_id` BIGINT,
+	IN `inp_status` INT(3),
+	IN `inp_statusText` TEXT
+)
+BEGIN 
+	INSERT INTO responses(entry_id,status,statusText) VALUES
+	(inp_entry_id,inp_status,inp_statusText);
+
+	SELECT MAX(response_id) AS response_id FROM responses WHERE entry_id = `inp_entry_id`;
+END$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS get_files;
+DELIMITER $
+CREATE PROCEDURE `get_files`(
+	IN `inp_user_id` BIGINT
+)
+	BEGIN SELECT * FROM files WHERE user_id=`inp_user_id`;
+END$
+DELIMITER ;
 
--- model/save_file.php
+++ model/save_file.php
@@ -1,12 +1,7 @@
-<?
-
-use function PHPSTORM_META\type;
-
+<?php
 session_start();
+
 include "../model/connection_db.php";
-if (!isset($_SESSION['email'])) {
-    header('Location: ../view/home_user/home.php');
-}
 
 $user_id = $_SESSION['user_id'];
 $file_name = $_POST['name'];
@@ -15,132 +10,147 @@ $upload_isp = $_SESSION['isp'];
 $upload_location = $_SESSION['city'];
 $text_contents = $_POST['contents'];
 
-$query = mysqli_query($conn, "INSERT INTO files(user_id,name,size,upload_isp,upload_location,full_file) VALUES('$user_id','$file_name','$file_size','$upload_isp','$upload_location','$text_contents')") or die(mysqli_error($conn));
 
-$query = mysqli_query($conn, "SELECT MAX(file_id) AS file_id FROM files WHERE user_id = $user_id");
+$query = mysqli_query($conn, "CALL add_file('$user_id','$file_name','$file_size','$upload_isp','$upload_location','$text_contents')") or die(mysqli_error($conn));
 
-$res = mysqli_fetch_array($query);
-$file_id = $res['file_id'];
+$result = mysqli_fetch_array($query);
 
+$file_id =  $result['file_id'];
+
+$query->close();
+$conn->next_result();
 
 $json_contents = json_decode($text_contents);
 $entries = $json_contents->contents;
 
 foreach ($entries as $entry) {
-
     if (array_key_exists('startedDateTime', $entry)) {
-        $_startedDateTime = $entry->startedDateTime;
+        $_startedDateTime = "$entry->startedDateTime";
     } else {
-        $_startedDateTime = "0000-00-00 00:00:00";
+        $_startedDateTime = "NULL";
     }
 
     if (array_key_exists('serverIpAddress', $entry)) {
-        $_serverIpAddress = $entry->serverIpAddress;
+        $_serverIpAddress = "$entry->serverIpAddress";
     } else {
-        $_serverIpAddress = 0;
+        $_serverIpAddress = "NULL";
     }
 
     if (array_key_exists('wait', $entry)) {
         $_wait = $entry->wait;
     } else {
-        $_wait = 0;
+        $_wait = "NULL";
     }
 
-    $sql = mysqli_query($conn, "INSERT INTO entries(file_id,startedDateTime,serverIpAddress,wait) VALUES ('$file_id','$_startedDateTime','$_serverIpAddress','$_wait')") or die(mysqli_error($conn));
-
-    $sql = mysqli_query($conn, "SELECT MAX(entry_id) AS entry_id FROM entries WHERE file_id = $file_id");
-
+    // ------------===--- ADD ENTRY ----------------
+    $sql = mysqli_query($conn, "CALL add_entry('$file_id','$_startedDateTime',$_serverIpAddress,$_wait)") or die(mysqli_error($conn));
 
     $res = mysqli_fetch_array($sql);
-
     $entry_id = $res['entry_id'];
+
+    $sql->close();
+    $conn->next_result();
+    // ----------------ADD ENTRY'S REQUEST ---------
 
     $_method = $entry->request->method;
     $_url = $entry->request->url;
 
-    $sql = mysqli_query($conn, "INSERT INTO requests(entry_id,method,url) VALUES ('$entry_id','$_method','$_url')") or die(mysqli_error($conn));
-
-
-    $sql = mysqli_query($conn, "SELECT MAX(request_id) AS request_id FROM requests WHERE entry_id=$entry_id");
+    $sql = mysqli_query($conn, "CALL add_request('$entry_id','$_method','$_url')") or die(mysqli_error(($conn)));
 
     $res = mysqli_fetch_array($sql);
-
     $request_id = $res['request_id'];
 
+    $sql->close();
+    $conn->next_result();
+    $column_name = "request_id";
     $headers = $entry->request->headers;
+    add_headers($conn, $column_name, $request_id, $headers);
 
-    $type = 'request_id';
-
-    add_headers($conn, $type, $request_id, $headers);
+    // ----------------ADD ENTRY'S RESPONSE ---------
 
     $_status = $entry->response->status;
     $_statusText = $entry->response->statusText;
 
-    
 
-    $sql = mysqli_query($conn, "INSERT INTO responses(entry_id,status,statusText) VALUES ($entry_id,'$_status','$_statusText')") or die(mysqli_error($conn));
+    if ($_statusText == "") {
+        $_statusText = "Invalid response";
+    }
+    if ($_status == 0) {
+        $_status = "NULL";
+    }
 
-    $sql = mysqli_query($conn, "SELECT MAX(response_id) AS response_id FROM responses WHERE entry_id = $entry_id") or die(mysqli_error($conn));
+
+    $sql = mysqli_query($conn, "CALL add_response('$entry_id',$_status,'$_statusText')") or die(mysqli_error($conn));
 
     $res = mysqli_fetch_array($sql);
-
     $response_id = $res['response_id'];
 
+    $sql->close();
+    $conn->next_result();
+
+    $column_name = "response_id";
     $headers = $entry->response->headers;
-
-    $type = "response_id";
-
-    add_headers($conn, $type, $response_id, $headers);
+    add_headers($conn, $column_name, $response_id, $headers);
 }
-
 
 function add_headers($conn, $type, $r_id, $headers)
 {
+    $all_null = 1;
     foreach ($headers as $header) {
         if (array_key_exists('content_type', $header)) {
-            $_content_type = $header->content_type;
+            $_content_type = "'$header->content_type'";
+            $all_null = 0;
         } else {
             $_content_type = "NULL";
         }
 
         if (array_key_exists('cache_control', $header)) {
-            $_cache_control = $header->cache_control;
+            $_cache_control = "'$header->cache_control'";
+            $all_null = 0;
         } else {
             $_cache_control = "NULL";
         }
 
         if (array_key_exists('pragma', $header)) {
-            $_pragma = $header->pragma;
+            $_pragma = "'$header->pragma'";
+            $all_null = 0;
         } else {
             $_pragma = "NULL";
         }
 
         if (array_key_exists('expires', $header)) {
-            $_expires = $header->expires;
+            $_expires = "'$header->expires'";
+            $all_null = 0;
         } else {
             $_expires = "NULL";
         }
 
         if (array_key_exists('age', $header)) {
             $_age = $header->age;
+            $all_null = 0;
         } else {
             $_age = "NULL";
         }
 
         if (array_key_exists('last_modified', $header)) {
-            $_last_modified = $header->last_modified;
+            $_last_modified = "'$header->last_modified'";
+            $all_null = 0;
         } else {
             $_last_modified = "NULL";
         }
 
 
         if (array_key_exists('host', $header)) {
-
-            $_host = $header->host;
+            $_host = "'$header->host'";
+            $all_null = 0;
         } else {
             $_host = "NULL";
         }
-        
-        $sql = mysqli_query($conn, "INSERT INTO headers($type,content_type,pragma,expires,age,last_modified,host,cache_control) VALUES ($r_id,'$_content_type','$_pragma',$_expires,$_age,'$_last_modified', '$_host', '$_cache_control')") or die(mysqli_error($conn));
+
+        if ($all_null == 0) {
+            continue;
+        }
+
+        $sql = mysqli_query($conn, "INSERT INTO headers ($type,content_type,pragma,expires,age,last_modified,host,cache_control) VALUES ($r_id,$_content_type,$_pragma,$_expires,$_age,$_last_modified, $_host, $_cache_control)") or die(mysqli_error($conn));
     }
 }
