d5e5c0ac740069fe56cdafc16950ba1bb8b5568a
unknown <k.moragemos99@gmail.com>
2021-08-20 17:30:13 +0300

    allages sto admin

--- model/admininfo.php
+++ model/admininfo.php
@@ -1,16 +1,56 @@
 <?php
+session_start();
+include '../model/connection_db.php';
 
 
-$numbers = 
+//
+
+if ($_POST['type'] == "regs")
+{
+   
+    $query = "SELECT COUNT(user_id) AS user_count FROM users ";
+    $query_run = mysqli_query($conn, $query);
+    $user_count = mysqli_fetch_array($query_run);
+    $user_count = $user_count["user_count"];
+    echo $user_count;
+}
 // session_start();
 // include ("../model/connection_db.php");
 
 
-//     $query = "SELECT id FROM user ORDER BY id ";
-//     $query_run = mysqli_query($conn, $query);
-//     $row= mysqli_num_rows($query_run);
-//     echo $row;
-  
+if ($_POST['type'] == "methods")
+{
+    
+    $query = "SELECT method ,count(*) FROM requestss WHERE method='GET' ";
+    $query_run = mysqli_query($conn, $query);
+    // $method_count = mysqli_fetch_array($query_run);
+    $row = mysqli_fetch_assoc($query_run);
+    if($row == 0)
+      echo "woj";
+      else
+foreach($row  as $value){
+    //Print the element out.
+    echo $value, '<br>';
+}
+
+
+$query = "SELECT method ,count(*) FROM requestss WHERE method='POST' ";
+$query_run = mysqli_query($conn, $query);
+// $method_count = mysqli_fetch_array($query_run);
+$row = mysqli_fetch_assoc($query_run);
+if($row == 0)
+  echo "woj";
+  else
+foreach($row  as $value){
+//Print the element out.
+echo $value, '<br>';
+}
+
+    
+    // $method_count = mysqli_fetch_array($query_run);
+    // $method_count = $method_count["method_count"];
+    // echo $method_count;
+} 
 
 
 
